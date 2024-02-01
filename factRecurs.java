4ea61c76ea0d18b84f8b0a71a485ab38fe4a5678
Prasad Alai <110602321+321prasad0058@users.noreply.github.com>
2024-02-01 17:36:32 +0530

    Add files via upload

--- factRecurs.java
+++ factRecurs.java
@@ -1,19 +1,26 @@
 import java.util.*;
 
-public class factRecurs
+public class factRecurs 
 {
-    public static void main(String []args)
+ public static int fact(int n)
+ {
+    if(n==0)
     {
-        factRecurs r = new factRecurs();
-        // System.out.println(r.fact(5));
-        r.fact(5);
+    return 1;
     }
+    int fnm1 = fact(n-1);
+    int f = n * fnm1;
+    return f;
+ }  
 
-    public static int fact(int n)
-    {
-        int fact_nm1 = fact(n-1);
-        int fact_n = n * fact(n-1);
-        return fact_n;
-    } 
-    
+ public static void main(String[] args)
+ {
+    int n =5;
+    fact(n);
+    System.out.println(fact(n));
+ }
+
+ 
+
+
 }
